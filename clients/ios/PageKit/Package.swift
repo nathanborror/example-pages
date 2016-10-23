77eb2905452f309122b7cab6d687977d831864f6
Nathan Borror <nathan@nathanborror.com>
2016-10-22 21:46:10 -0700

    Ignoring Packages/ and being more specific about what versions of packages I'm importing.

--- clients/ios/PageKit/Package.swift
+++ clients/ios/PageKit/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "PageKit",
     dependencies: [
-        .Package(url: "https://github.com/apple/swift-protobuf.git", majorVersion: 0),
-        .Package(url: "https://github.com/nathanborror/swift-grpc.git", majorVersion: 0),
+        .Package(url: "https://github.com/nathanborror/swift-protobuf.git", Version(0, 9, 25)),
+        .Package(url: "https://github.com/nathanborror/swift-grpc.git", Version(0, 1, 11)),
     ]
 )
